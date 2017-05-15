c5c11f73dd24bb36e922a879e4cdff64420d6609
AmFlint <amasselot@hotmail.com>
2017-05-15 14:26:53 +0200

    update bdd + backoffice

--- core/config/db.sql
+++ core/config/db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Lun 15 Mai 2017 à 12:03
+-- Généré le :  Lun 15 Mai 2017 à 14:26
 -- Version du serveur :  5.5.42
 -- Version de PHP :  7.0.8
 
@@ -27,7 +27,14 @@ CREATE TABLE `actualites` (
   `content_short` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content_full` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contenu de la table `actualites`
+--
+
+INSERT INTO `actualites` (`id`, `title`, `slug`, `content_short`, `content_full`, `date`) VALUES
+  (1, 'L''Architecture Moderne', 'architecture-moderne', 'Au cours des siècles, l''Homme a su montrer une certaine maîtrise de l''architecture, en effet ...', 'Le full content qu''on va utiliser pour cette première actualité', '2017-05-15 10:12:38');
 
 -- --------------------------------------------------------
 
@@ -74,6 +81,27 @@ CREATE TABLE `contact` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `location`
+--
+
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL,
+  `location` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contenu de la table `location`
+--
+
+INSERT INTO `location` (`id`, `location`) VALUES
+  (1, 'Val de Loire'),
+  (2, 'Saint-Barthelemy'),
+  (3, 'Belgique'),
+  (4, 'Alsace');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `magazines`
 --
 
@@ -81,9 +109,54 @@ CREATE TABLE `magazines` (
   `id` int(11) NOT NULL,
   `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `synopsis` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `content_full` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `image` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `link` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `location_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contenu de la table `magazines`
+--
+
+INSERT INTO `magazines` (`id`, `title`, `synopsis`, `image`, `date`, `link`, `location_id`) VALUES
+  (1, 'Centre Val de Loire', 'L''Architecture dans le Centre Val de Loire est très maîtrisée...', 'magazine_276.jpg', '2017-05-15', 'http://fr.calameo.com/read/001121299ba572cf53fca', 1),
+  (2, 'Belgique', 'L''Architecture au coeur de la Belgique, une aventure ...', 'magazine_278.jpg', '2017-05-15', 'http://fr.calameo.com/read/00112129970991e922bc3', 3),
+  (3, 'Alsace', 'L''Alsace et l''architecture sa fait 1', 'magazine_279.jpg', '2017-05-15', 'http://fr.calameo.com/read/0011212993e4406f651e8', 4),
+  (4, 'Saint-Barthelemy', 'L''Architecture dans la ville de Saint-Barthelemy', 'magazine_270.jpg', '2017-05-15', 'http://fr.calameo.com/read/0011212990739cc3f4df6', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `partenaires`
+--
+
+CREATE TABLE `partenaires` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `location_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contenu de la table `partenaires`
+--
+
+INSERT INTO `partenaires` (`id`, `name`, `location_id`) VALUES
+  (1, 'Belgique Ciment', 3),
+  (2, 'Architecte Alsace', 4),
+  (3, 'Architecture de Saint-Barthelemy', 2),
+  (4, 'Val de Loire archi', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `partenariat`
+--
+
+CREATE TABLE `partenariat` (
+  `id` int(11) NOT NULL,
+  `partenaire_id` int(11) NOT NULL,
+  `magazine_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -102,8 +175,8 @@ CREATE TABLE `type_commande` (
 --
 
 INSERT INTO `type_commande` (`id`, `type`) VALUES
-(1, 'commande'),
-(2, 'abonnement');
+  (1, 'commande'),
+  (2, 'abonnement');
 
 --
 -- Index pour les tables exportées
@@ -128,9 +201,27 @@ ALTER TABLE `contact`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `magazines`
 --
 ALTER TABLE `magazines`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `partenaires`
+--
+ALTER TABLE `partenaires`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `partenariat`
+--
+ALTER TABLE `partenariat`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -147,7 +238,7 @@ ALTER TABLE `type_commande`
 -- AUTO_INCREMENT pour la table `actualites`
 --
 ALTER TABLE `actualites`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `commande`
 --
@@ -159,9 +250,24 @@ ALTER TABLE `commande`
 ALTER TABLE `contact`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT pour la table `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT pour la table `magazines`
 --
 ALTER TABLE `magazines`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `partenaires`
+--
+ALTER TABLE `partenaires`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT pour la table `partenariat`
+--
+ALTER TABLE `partenariat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `type_commande`
