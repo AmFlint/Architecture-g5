e01bb19c057e2afd6818291249dfd7dce83a2e2b
AmFlint <amasselot@hotmail.com>
2017-05-15 12:00:55 +0200

    ajout controller admin et connexion pdo dans parent Model

--- core/classes/Model.php
+++ core/classes/Model.php
@@ -5,5 +5,15 @@ namespace classes;
 
 abstract class Model
 {
+    private $db;
 
+    public function __construct()
+    {
+        try {
+            $this->db = new PDO(DBN,DB_USER,DB_PWD);
+        } catch(PDOException $exception) {
+            die($exception->getMessage());
+        }
+        $this->db->exec("SET NAMES UTF8");
+    }
 }
