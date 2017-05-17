a5ace9abe842cc84e08a7df2d34c07aa73aefddc
Julie Manhes <julie.manhes@hetic.net>
2017-05-17 14:31:18 +0200

    nouvelle version

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -26,30 +26,7 @@ for (let i = 0; i < lis.length; i++) {
 
 
 /**** changement nav ******/
-var nav = document.querySelector('.main-nav');
-var logo = document.querySelector('.logo');
-var lesa = document.querySelectorAll('nav a');
 
-document.addEventListener('scroll', function (event) {
-    if (document.body.scrollTop > 80 || event.pageY > 80) {
-        nav.classList.add('colornav');
-        tab.style.background = '#fff';
-        logo.src = "assets/img-layout/black-logo.svg";
-        for (var i = 0; i < lesa.length; i++) {
-            lesa[i].style.color = "#000";
-
-        }
-
-    } else if (document.body.scrollTop < 80 || event.pageY < 80) {
-        nav.classList.remove('colornav');
-        tab.style.background = '#15D1E7';
-        logo.src = "assets/img-layout/logo.svg";
-        for (var i = 0; i < lesa.length; i++) {
-            lesa[i].style.color = "#fff";
-        }
-
-    }
-});
 
 /**** changement infos sur les parutions ******/
 
